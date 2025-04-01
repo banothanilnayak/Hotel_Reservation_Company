deda940bf3e685639d04cb79722bab7718dc36b2
Banoth Anil Nayak <110718804+banothanilnayak@users.noreply.github.com>
2025-04-01 21:24:15 +0530

    Edit cabin and insert cabin feature (#5)
    * create cabin bug fixed
    
    * image rollbak if failed fix
    
    * cabin edit and adding issue resolved
    
    * added space done nothing

--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -2,10 +2,10 @@ import supabase, { supabaseUrl } from "./supabase";
 
 export async function getAllCabins() {
   const { data, error } = await supabase.from("cabins").select("*");
-  console.log("getAllCabins", error);
   if (error) throw new Error("failed fetching the cabins..");
   return data;
 }
+
 
 export async function deleteCabin(id) {
   const { data, error } = await supabase.from("cabins").delete().eq("id", id);
@@ -17,30 +17,49 @@ export async function deleteCabin(id) {
 }
 
 //https://tbcqrlrgetvgxzialkex.supabase.co/storage/v1/object/public/cabins//cabin-001.jpg
-export async function createEditCabin(newObj) {
-  console.log("newObj pritnting", newObj);
+export async function createEditCabin(newObj, editID = "") {
+
+  const hasImagePath = newObj?.image?.startsWith?.(supabase);
+
   //creating a cabin
   const imgName = `${Math.random()}-${newObj.image.name}`.replaceAll("/", "");
-  const imgPath = `${supabaseUrl}/storage/v1/object/public/cabins/${imgName}`;
 
-  const { data: newData, error } = await supabase
-    .from("cabins")
-    .insert([{ ...newObj, image: imgPath }])
-    .select()
-    .single();
+  const imgPath = hasImagePath
+    ? newObj.image
+    : `${supabaseUrl}/storage/v1/object/public/cabins/${imgName}`;
 
-  console.log("newdata image url", newData);
-  //uploading image
+  //reusing the query
+  let query = supabase.from("cabins");
+
+  if (!editID) {
+    //if no edit id then insert data else edit data
+    query = query
+      .insert([{ ...newObj, image: imgPath }])
+      .select()
+      .single();
+  } else {
+    // if edit id present then update cabin using editID
+    query = query
+      .update({ ...newObj, image: imgPath })
+      .eq("id", editID)
+      .select()
+      .single();
+  }
+
+  //executing query based on condition
+  const { data: newData, error } = await query;
+
+  //uploading image to the storage once the text data is successfully stored inside the cabins cable
   const { error: uploadError } = await supabase.storage
     .from("cabins")
-    .upload(imgName, newData[0].image);
+    .upload(imgName, newData.image);
 
   //if any error while uploading then delete the cabin
   if (uploadError) {
     const { data, error } = await supabase
       .from("cabins")
       .delete()
-      .eq("id", newData[0].id);
+      .eq("id", newData.id);
     console.error(uploadError);
     throw new Error("cabin couldnt  created and failed to upload image ");
   }
